fb582ef52ed51553e74cb6817a1acb5464c3dfd6
MDRX1800 <mdrx1800@gmail.com>
2021-11-24 17:39:51 +0500

    f23

--- main/credetionals.py
+++ main/credetionals.py
@@ -1,3 +1,3 @@
 TOKEN = '2102703868:AAEfWQL4nj6ggo2V_vKHVecgmSeP9T0u_7g'
-URL = 'https://serbotbot.herokuapp.com/getpost/'
+URL = 'https://serbotbot1.herokuapp.com/getpost/'
 BOT_API = f'https://api.telegram.org/bot{TOKEN}/'
