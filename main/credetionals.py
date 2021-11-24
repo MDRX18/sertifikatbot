b84ba8b0a44cb84bb78bbc9bd7c7c59ea9b70797
MDRX1800 <mdrx1800@gmail.com>
2021-11-24 16:50:44 +0500

    f

--- main/credetionals.py
+++ main/credetionals.py
@@ -1,3 +1,3 @@
 TOKEN = '2102703868:AAEfWQL4nj6ggo2V_vKHVecgmSeP9T0u_7g'
-URL = 'https://sertifikatbot.herokuapp.com/getpost/'
+URL = 'https://serbotbot.herokuapp.com/getpost/'
 BOT_API = f'https://api.telegram.org/bot{TOKEN}/'
